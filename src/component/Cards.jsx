0fd39a7487e228cd27f0862973674ffbbec8f2f3
nahidhasan330874 <nahidhasan1005004@gmail.com>
2026-03-31 00:24:57 +0600

    added the models card and set the new file

--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -1,10 +1,11 @@
 import { use } from "react";
+import ModelsCard from "./ModelsCard";
 
  
 const Cards = ({ cardsData }) => {
     const cards = use(cardsData);
     console.log(cards);
-    const [isCardsData, setIsCardsData] = useState([false]);
+   
     return (
         <div className="py-20 max-w-7xl mx-auto ">
            {/* card heading text */}
@@ -22,35 +23,9 @@ const Cards = ({ cardsData }) => {
             {/* cards */}
             <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 ">
                 {
-                    cards.map((card) => ( <div className=" card p-5 rounded-2xl shadow-lg border overflow-hidden border-gray-200">
-                        <div className="flex flex-col justify-between">
-                           <p className="flex justify-end ">  <span className=" inline-flex items-center bg-red-500/10 border border-red-500/30 text-red-400 text-sm font-medium px-4 py-1 rounded-full border-none">{card.tag}</span></p>
-
-                            <div className=" flex justify-between ">
-                                <img src={card.image} alt="Shoes"  className="  rounded-full w-12"/>
-                               
-                            </div>
-                            <div className="card-body space-y-3">
-                                 <h2 className="font-bold text-2xl">{card.name}</h2>
-                                <p  className=" line-clamp-2 text-[#627382]">{card.description}</p>
-                                <p> <span className="font-bold text-2xl">${card.price}</span>/{card.period}</p>
-
-                                <div>   
-                                    <ul className="list-inside list-none space-y-1  text-[#627382] ">
-                                        {card.features.map((feature, index) => (
-                                           <li  key={index}> <span className="text-green-500"> <i class="fa-solid fa-check"></i></span> {feature}</li>
-                                        ))}
-                                    </ul>
-
-                                </div>
-                                 
-                                <button onClick={() => setIsCardsData(!isCardsData)}  className="btn bg-linear-to-r from-red-400 via-orange-400 to-amber-400 text-white rounded-4xl shadow-xl shadow-red-500/30 border-none"> { isCardsData ? 'Added to Cart' : 'Add to Cart'  }</button>
-                                
-                            </div>
-
-                        </div>
-
-                    </div>
+                    cards.map((card) => ( 
+                   
+                        <ModelsCard key={card.id} card={card}/>
                      
                 ))
                 }
---
+++ src/component/ModelsCard.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+const ModelsCard = ({ card }) => {
+     const [isCardsData, setIsCardsData] = useState(false);
+     const handleAddToCart = () => {
+        setIsCardsData(true);
+     }
+    return (
+          <div className=" card p-5 rounded-2xl shadow-lg border overflow-hidden border-gray-200">
+                        <div className="flex flex-col justify-between">
+                           <p className="flex justify-end ">  <span className=" inline-flex items-center bg-red-500/10 border border-red-500/30 text-red-400 text-sm font-medium px-4 py-1 rounded-full border-none">{card.tag}</span></p>
+
+                            <div className=" flex justify-between ">
+                                <img src={card.image} alt="Shoes"  className="  rounded-full w-12"/>
+                               
+                            </div>
+                            <div className="card-body space-y-3">
+                                 <h2 className="font-bold text-2xl">{card.name}</h2>
+                                <p  className=" line-clamp-2 text-[#627382]">{card.description}</p>
+                                <p> <span className="font-bold text-2xl">${card.price}</span>/{card.period}</p>
+
+                                <div>   
+                                    <ul className="list-inside list-none space-y-1  text-[#627382] ">
+                                        {card.features.map((feature, index) => (
+                                           <li  key={index}> <span className="text-green-500"> <i class="fa-solid fa-check"></i></span> {feature}</li>
+                                        ))}
+                                    </ul>
+
+                                </div>
+                                 
+                                <button onClick={handleAddToCart}  className="btn bg-linear-to-r from-red-400 via-orange-400 to-amber-400 text-white  rounded-4xl shadow-xl shadow-red-500/30 border-none"> { isCardsData ? 'Added to Cart' : 'Buy Now'  }</button>
+                                
+                            </div>
+
+                        </div>
+
+                    </div>
+    );
+};
+
+export default ModelsCard;
